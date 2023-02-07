3993d801d8c350ce5a0f86763aab4bdfc0f19640
Esttrella <lpzlpz2022@gmail.com>
2023-02-07 14:56:18 -0600

    Modificando el modelo principal de la tienda

--- TiendaTennis/Models/TiendaTennisContext.cs
+++ TiendaTennis/Models/TiendaTennisContext.cs
@@ -1,11 +1,26 @@
-﻿using System;
+﻿/*
+ * Autor: Hermilo Amaraz Vargas
+ * Fecha de creación: 05/02/2023
+ * Fecha de modificación: 05/02/2023
+ * Descripción: Se crea el modelo para productos
+ */
+
+/*
+ * Se usan las librerias del sistema y la generica de colección, tambien se manda a llamar una libreria
+ */
+using System;
 using System.Collections.Generic;
 using Microsoft.EntityFrameworkCore;
-
+/*
+ * Se declara un ambito de objetos relacionados
+ */
 namespace TiendaTennis.Models;
 
 public partial class TiendaTennisContext : DbContext
 {
+    /*
+     * se crea la tienda del contexto
+     */
     public TiendaTennisContext()
     {
     }
@@ -20,16 +35,14 @@ public partial class TiendaTennisContext : DbContext
     public virtual DbSet<Producto> Productos { get; set; }
 
     public virtual DbSet<Usuario> Usuarios { get; set; }
-
-    //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-//#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-  //      => optionsBuilder.UseSqlServer("server=LAPTOP-9GV7U539\\SQLEXPRESS; database=TiendaTennis; integrated security=true; TrustServerCertificate=true;");
-
+    /*
+     * se crea el metodo de tipo void donde se traen todos los datos y se les da un tamaño y se validan del cliente
+     */
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.Entity<Cliente>(entity =>
         {
-            entity.HasKey(e => e.Id).HasName("PK__Clientes__3214EC07B5E863B2");
+            entity.HasKey(e => e.Id).HasName("PK_Clientes_3214EC07B5E863B2");
 
             entity.Property(e => e.Apellido)
                 .HasMaxLength(50)
@@ -50,10 +63,12 @@ public partial class TiendaTennisContext : DbContext
                 .HasMaxLength(50)
                 .IsUnicode(false);
         });
-
+        /*
+         * se traen los datos del producto del entity con su tamaño y su nombre
+         */
         modelBuilder.Entity<Producto>(entity =>
         {
-            entity.HasKey(e => e.Id).HasName("PK__Producto__3214EC0715F2BC98");
+            entity.HasKey(e => e.Id).HasName("PK_Producto_3214EC0715F2BC98");
 
             entity.Property(e => e.Descripcion)
                 .HasMaxLength(50)
@@ -67,10 +82,12 @@ public partial class TiendaTennisContext : DbContext
                 .IsUnicode(false)
                 .HasColumnName("Producto");
         });
-
+        /*
+         * se traen los datos del usuario del entity y se les da un tamaño y que no pueden ser nulos.
+         */
         modelBuilder.Entity<Usuario>(entity =>
         {
-            entity.HasKey(e => e.Id).HasName("PK__Usuarios__3214EC0797722F61");
+            entity.HasKey(e => e.Id).HasName("PK_Usuarios_3214EC0797722F61");
 
             entity.Property(e => e.Apellido)
                 .HasMaxLength(50)
@@ -91,6 +108,8 @@ public partial class TiendaTennisContext : DbContext
 
         OnModelCreatingPartial(modelBuilder);
     }
-
+    /*
+    * se crea un modelo de creación parcial de tipo void
+    */
     partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
 }
