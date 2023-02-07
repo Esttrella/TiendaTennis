ec86edee4772d19e0baf19a4a99f0e1564489fa3
Esttrella <lpzlpz2022@gmail.com>
2023-02-07 13:00:56 -0600

    creacion del modelo para productos

---
+++ TiendaTennis/Models/Producto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TiendaTennis.Models;
+
+public partial class Producto
+{
+    public int Id { get; set; }
+
+    public string? Producto1 { get; set; }
+
+    public double? Precio { get; set; }
+
+    public int? Stok { get; set; }
+
+    public string? Descripcion { get; set; }
+
+    public string? Path { get; set; }
+}
