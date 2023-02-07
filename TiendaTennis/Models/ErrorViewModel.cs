9245d96628a08910e82b1a1b8dc0826e2ad185d2
Esttrella <lpzlpz2022@gmail.com>
2023-02-07 13:05:30 -0600

    agregando un controler para excepciones

---
+++ TiendaTennis/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TiendaTennis.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
