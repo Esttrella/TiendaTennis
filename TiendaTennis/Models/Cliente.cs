3eb36d795756116ec1002d16ed63c3abc6b776e0
Esttrella <lpzlpz2022@gmail.com>
2023-02-07 14:47:16 -0600

    Modificando el modelo de clientes

--- TiendaTennis/Models/Cliente.cs
+++ TiendaTennis/Models/Cliente.cs
@@ -1,8 +1,23 @@
-﻿using System;
-using System.Collections.Generic;
+﻿/*
+ * Autor: Iris Estrella Lopez Lopez
+ * Fecha de creación: 04/02/2023
+ * Fecha de modificación: 04/02/2023
+ * Descripción: Se crea el modelo del modulo cliente
+ */
 
+/*
+ * Se usan las librerias del sistema y la generica de colección
+ */
+using System;
+using System.Collections.Generic;
+/*
+ * Se declara un ambito de objetos relacionados
+ */
 namespace TiendaTennis.Models;
 
+/*
+ * se crea la clase cliente donde se hacen los set y los get de cada uno de los datos
+ */
 public partial class Cliente
 {
     public int Id { get; set; }
