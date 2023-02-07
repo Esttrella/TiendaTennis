3bcfd76458ed0dfe0b6e6b314c59b56356cd59da
Esttrella <lpzlpz2022@gmail.com>
2023-02-07 12:59:14 -0600

    creando modelo para clientes

---
+++ TiendaTennis/Models/Cliente.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TiendaTennis.Models;
+
+public partial class Cliente
+{
+    public int Id { get; set; }
+
+    public string? Nombre { get; set; }
+
+    public string? Apellido { get; set; }
+
+    public string? Correo { get; set; }
+
+    public string? Contraseña { get; set; }
+
+    public string? Direccion { get; set; }
+
+    public string? Telefono { get; set; }
+}
