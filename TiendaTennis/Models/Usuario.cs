26c49f5ac95137c3e1f33d072f93932b24041397
Esttrella <lpzlpz2022@gmail.com>
2023-02-07 14:57:27 -0600

    Modificando el modelo de usuarios

--- TiendaTennis/Models/Usuario.cs
+++ TiendaTennis/Models/Usuario.cs
@@ -1,8 +1,22 @@
-﻿using System;
+﻿/*
+ * Autor: Heber Esaú Hernández Ramírez
+ * Fecha de creación: 05/02/2023
+ * Fecha de modificación: 05/02/2023
+ * Descripción: Se crea el modelo para productos
+ */
+
+/*
+ * Se usan las librerias del sistema y la generica de colección
+ */
+using System;
 using System.Collections.Generic;
-
+/*
+ * Se declara un ambito de objetos relacionados
+ */
 namespace TiendaTennis.Models;
-
+/*
+ * se crea la clase usuario donde se hacen los set y los get de cada uno de los datos
+ */
 public partial class Usuario
 {
     public int Id { get; set; }
