89c46d252e118caa66aba0d04ec42421c033324a
Esttrella <lpzlpz2022@gmail.com>
2023-02-07 14:50:44 -0600

    Modificando el modelo del producto

--- TiendaTennis/Models/Producto.cs
+++ TiendaTennis/Models/Producto.cs
@@ -1,8 +1,22 @@
-﻿using System;
+﻿/*
+ * Autor: Heber Esaú Hernández Ramírez
+ * Fecha de creación: 05/02/2023
+ * Fecha de modificación: 05/02/2023
+ * Descripción: Se crea el modelo para productos
+ */
+
+/*
+ * Se usan las librerias del sistema y la generica de colección
+ */
+using System;
 using System.Collections.Generic;
-
+/*
+ * Se declara un ambito de objetos relacionados
+ */
 namespace TiendaTennis.Models;
-
+/*
+ * Se hace la clase producto donde se hacen los set y get de cada uno de los datos
+ */
 public partial class Producto
 {
     public int Id { get; set; }
